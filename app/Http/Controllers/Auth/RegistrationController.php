d03ddc82d2478d7976a4ee806c3c10d44bd3dec8
Raymart Magallanes <magallanesraymart@gmail.com>
2025-03-17 00:08:23 +0800

    added welcome mail

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\WelcomeMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class RegistrationController extends Controller
 {
@@ -30,9 +32,12 @@ class RegistrationController extends Controller
                 'password' => Hash::make($validated['password']),
             ]);
             
+            // Should send email first before redirecting
+            Mail::to($user->email)->send(new WelcomeMail($user));
+            
             // Ma login ang user after successfully registering
             Auth::login($user);
             
-            return redirect()->route('records.index')->with('success', "Registration Successful! Welcome, {$user->name}.");
+            return redirect()->route('records.index')->with('success', "Registration successful! Welcome, {$user->name}.");
     }
 }
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome Mail</title>
+</head>
+<body>
+    <h1>Greetings, {{ $user->name }}</h1>
+    <p>Welcome to our application. We're glad to have you on board!</p>
+    <p>Feel free to explore and contact us if you have any questions.</p>
+    <p>Best regards, <br> The Admin</p>
+</body>
+</html>
